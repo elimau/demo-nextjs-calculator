3721cebdea3a15bc1ecc8572965320d3698220d1
elimau <saltydogau@gmail.com>
2024-04-22 19:52:51 +1000

    calculator: unit testing and logic fixes

--- calculator/src/components/calculator/index.test.tsx
+++ calculator/src/components/calculator/index.test.tsx
@@ -4,165 +4,202 @@ import Page from './index'
 
 describe('Calculator', () => {
   it('Will strip leading 0s in integer numbers and calculation works', async () => {
-    // Inputs and xpectations
+    // Inputs and expectations
     const input = `03${BUTTON.OP_MULTIPLY}03`
     const outputDisplay = `3${BUTTON.OP_MULTIPLY}3`
     const outputCalculation = '9'
 
     // Run and test
     render(<Page />)
-    const clickSequence = Array.from(input)
     const displayElement = screen.getByTestId(TESTID.DISPLAY)
     const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // Do calculation
+    const clickSequence = Array.from(input)
     for (let i = 0; i < clickSequence.length; i++) {
       fireEvent.click(getByText(buttonsElement, clickSequence[i]))
     }
     expect(displayElement.textContent).toBe(outputDisplay)
     fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
     expect(displayElement.textContent).toBe(outputCalculation)
-    console.log('test:', {
-      input,
-      outputDisplay,
-      outputCalculation,
-    })
+    // console.log('test:', {
+    //   input,
+    //   outputDisplay,
+    //   outputCalculation,
+    // })
   })
   it('Will not strip leading 0s in decimal numbers and calculation works', async () => {
-    // Inputs and xpectations
+    // Inputs and expectations
     const input = `0.3${BUTTON.OP_PLUS}0.3`
     const outputDisplay = `0.3${BUTTON.OP_PLUS}0.3`
     const outputCalculation = '0.6'
 
     // Run and test
     render(<Page />)
-    const clickSequence = Array.from(input)
     const displayElement = screen.getByTestId(TESTID.DISPLAY)
     const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // Do calculation
+    const clickSequence = Array.from(input)
     for (let i = 0; i < clickSequence.length; i++) {
       fireEvent.click(getByText(buttonsElement, clickSequence[i]))
     }
     expect(displayElement.textContent).toBe(outputDisplay)
     fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
     expect(displayElement.textContent).toBe(outputCalculation)
-    console.log('test:', {
-      input,
-      outputDisplay,
-      outputCalculation,
-    })
-  })
-  it('Chaining calculculations by using "=" and continuing with more operations works', async () => {
-    // // Inputs and xpectations
-    // const input1 = `${BUTTON.OP_AC}`
-    // const outputDisplay1 = `0`
-    // const outputCalculation1 = '0'
-    // const input2 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay2 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation2 = '0.6'
-    // const input3 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay3 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation3 = '0.6'
-
-    // // Run and test
-    // render(<Page />)
-    // const clickSequence = Array.from(input)
-    // const displayElement = screen.getByTestId(TESTID.DISPLAY)
-    // const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
-    // for (let i = 0; i < clickSequence.length; i++) {
-    //   fireEvent.click(getByText(buttonsElement, clickSequence[i]))
-    // }
-    // expect(displayElement.textContent).toBe(outputDisplay)
-    // fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
-    // expect(displayElement.textContent).toBe(outputCalculation)
     // console.log('test:', {
     //   input,
     //   outputDisplay,
     //   outputCalculation,
+    // })
+  })
+  it('Chaining calculculations by using "=" and continuing with more operations works', async () => {
+    // Inputs and expectations
+    const input1 = `3${BUTTON.OP_DIVIDE}8`
+    const outputDisplay1 = `3${BUTTON.OP_DIVIDE}8`
+    const outputCalculation1 = '0.375'
+    const input2 = `${BUTTON.OP_MINUS}99`
+    const outputDisplay2 = `${outputCalculation1}${BUTTON.OP_MINUS}99`
+    const outputCalculation2 = '-98.625'
+
+    // Run and test
+    render(<Page />)
+    const displayElement = screen.getByTestId(TESTID.DISPLAY)
+    const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // First calculation
+    let clickSequence = Array.from(input1)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay1)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation1)
+    // console.log('test:', {
+    //   input1,
+    //   outputDisplay1,
+    //   outputCalculation1,
+    // })
+
+    // Second calculation
+    clickSequence = Array.from(input2)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay2)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation2)
+    // console.log('test:', {
+    //   input2,
+    //   outputDisplay2,
+    //   outputCalculation2,
     // })
   })
   it('On click AC, will clear the display and next calculation is correct', async () => {
-    // // Inputs and xpectations
-    // const input1 = `${BUTTON.OP_AC}`
-    // const outputDisplay1 = `0`
-    // const outputAfterAc1 = '0'
-    // const input2 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay2 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputAfterAc2 = '0'
-    // const input3 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay3 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputAfterAc2 = '0'
+    // Inputs and expectations
+    const input1 = `3${BUTTON.OP_DIVIDE}`
+    const outputDisplay1 = `3${BUTTON.OP_DIVIDE}`
+    const outputAfterClickAc = '0'
+    const input2 = `4${BUTTON.OP_MULTIPLY}.25`
+    const outputDisplay2 = `4${BUTTON.OP_MULTIPLY}.25`
+    const outputCalculation2 = '1'
 
-    // // Run and test
-    // render(<Page />)
-    // const clickSequence = Array.from(input)
-    // const displayElement = screen.getByTestId(TESTID.DISPLAY)
-    // const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
-    // for (let i = 0; i < clickSequence.length; i++) {
-    //   fireEvent.click(getByText(buttonsElement, clickSequence[i]))
-    // }
-    // expect(displayElement.textContent).toBe(outputDisplay)
-    // fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
-    // expect(displayElement.textContent).toBe(outputCalculation)
+    // Run and test
+    render(<Page />)
+    const displayElement = screen.getByTestId(TESTID.DISPLAY)
+    const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // First calculation
+    let clickSequence = Array.from(input1)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay1)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_AC)) // Click AC
+    expect(displayElement.textContent).toBe(outputAfterClickAc)
     // console.log('test:', {
-    //   input,
-    //   outputDisplay,
-    //   outputCalculation,
+    //   input1,
+    //   outputDisplay1,
+    //   outputAfterClickAc,
+    // })
+
+    // Second calculation
+    clickSequence = Array.from(input2)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay2)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation2)
+    // console.log('test:', {
+    //   input2,
+    //   outputDisplay2,
+    //   outputCalculation2,
     // })
   })
-  it('On click "=" twice, will keep the display value and have no errors', async () => {
-    // // Inputs and xpectations
-    // const input1 = `${BUTTON.OP_AC}`
-    // const outputDisplay1 = `0`
-    // const outputCalculation1 = '0'
-    // const input2 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay2 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation2 = '0.6'
-    // const input3 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay3 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation3 = '0.6'
-
-    // // Run and test
-    // render(<Page />)
-    // const clickSequence = Array.from(input)
-    // const displayElement = screen.getByTestId(TESTID.DISPLAY)
-    // const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
-    // for (let i = 0; i < clickSequence.length; i++) {
-    //   fireEvent.click(getByText(buttonsElement, clickSequence[i]))
-    // }
-    // expect(displayElement.textContent).toBe(outputDisplay)
-    // fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
-    // expect(displayElement.textContent).toBe(outputCalculation)
+  it('On clicking multiple operation buttons, it will only keep the first operation and ignore the rest of the operation clicks, except minus just before the number (which is a negative number)', async () => {
+    // Inputs and xpectations
+    const input1 = `-6${BUTTON.OP_MULTIPLY}${BUTTON.OP_MULTIPLY}${BUTTON.OP_MULTIPLY}3`
+    const outputDisplay1 = `0-6${BUTTON.OP_MULTIPLY}3`
+    const outputCalculation1 = '-18'
+    // Run and test
+    render(<Page />)
+    const displayElement = screen.getByTestId(TESTID.DISPLAY)
+    const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // First calculation
+    let clickSequence = Array.from(input1)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay1)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation1)
     // console.log('test:', {
-    //   input,
-    //   outputDisplay,
-    //   outputCalculation,
+    //   input1,
+    //   outputDisplay1,
+    //   outputCalculation1,
     // })
   })
-  it('On click operation button more than once consecutively, will ignore the non-first operation clicks', async () => {
-    // // Inputs and xpectations
-    // const input1 = `${BUTTON.OP_AC}`
-    // const outputDisplay1 = `0`
-    // const outputCalculation1 = '0'
-    // const input2 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay2 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation2 = '0.6'
-    // const input3 = `${BUTTON.OP_AC}0.3${BUTTON.OP_PLUS}0.3`
-    // const outputDisplay3 = `0.3${BUTTON.OP_PLUS}0.3`
-    // const outputCalculation3 = '0.6'
-
-    // // Run and test
-    // render(<Page />)
-    // const clickSequence = Array.from(input)
-    // const displayElement = screen.getByTestId(TESTID.DISPLAY)
-    // const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
-    // for (let i = 0; i < clickSequence.length; i++) {
-    //   fireEvent.click(getByText(buttonsElement, clickSequence[i]))
-    // }
-    // expect(displayElement.textContent).toBe(outputDisplay)
-    // fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
-    // expect(displayElement.textContent).toBe(outputCalculation)
+  it('Works with negative numbers', async () => {
+    // Inputs and xpectations
+    const input1 = `-2${BUTTON.OP_MULTIPLY}-3${BUTTON.OP_MULTIPLY}-2`
+    const outputDisplay1 = `0-2${BUTTON.OP_MULTIPLY}-3${BUTTON.OP_MULTIPLY}-2`
+    const outputCalculation1 = '-12'
+    // Run and test
+    render(<Page />)
+    const displayElement = screen.getByTestId(TESTID.DISPLAY)
+    const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // First calculation
+    let clickSequence = Array.from(input1)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay1)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation1)
     // console.log('test:', {
-    //   input,
-    //   outputDisplay,
-    //   outputCalculation,
+    //   input1,
+    //   outputDisplay1,
+    //   outputCalculation1,
+    // })
+  })
+  it('Ignores operators within a negative number', async () => {
+    // Inputs and xpectations
+    const input1 = `-2${BUTTON.OP_MULTIPLY}-${BUTTON.OP_MULTIPLY}3${BUTTON.OP_MULTIPLY}-2`
+    const outputDisplay1 = `0-2${BUTTON.OP_MULTIPLY}-3${BUTTON.OP_MULTIPLY}-2`
+    const outputCalculation1 = '-12'
+    // Run and test
+    render(<Page />)
+    const displayElement = screen.getByTestId(TESTID.DISPLAY)
+    const buttonsElement = screen.getByTestId(TESTID.BUTTONS)
+    // First calculation
+    let clickSequence = Array.from(input1)
+    for (let i = 0; i < clickSequence.length; i++) {
+      fireEvent.click(getByText(buttonsElement, clickSequence[i]))
+    }
+    expect(displayElement.textContent).toBe(outputDisplay1)
+    fireEvent.click(getByText(buttonsElement, BUTTON.OP_EQUALS))
+    expect(displayElement.textContent).toBe(outputCalculation1)
+    // console.log('test:', {
+    //   input1,
+    //   outputDisplay1,
+    //   outputCalculation1,
     // })
   })
 })
--- calculator/src/components/calculator/utils.ts
+++ calculator/src/components/calculator/utils.ts
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from 'react'
-import { BUTTON } from './constants'
+import { ARITHMATIC_OPERATION_BUTTONS, BUTTON } from './constants'
 
 export const calculatorRun = ({
   displayValue,
@@ -10,40 +10,30 @@ export const calculatorRun = ({
   setDisplayValue: Dispatch<SetStateAction<string>>
   buttonValue: string
 }) => {
-  // console.log('displayValue/buttonValue:', {
-  //   displayValue,
-  //   buttonValue,
-  //   typeofButtonValue: typeof Number(buttonValue),
-  //   NumberButtonValue: Number(buttonValue),
-  // })
   let newDisplayValue
-
-  // Strip leading zeros in non-decimal numbers
   const lastCharOfDisplay = displayValue.charAt(displayValue.length - 1)
   if (!isNaN(Number(buttonValue)) && lastCharOfDisplay === BUTTON.NUM_0) {
-    setDisplayValue(displayValue.replace(/.$/, buttonValue))
-    return
+    // Strip leading zeros in non-decimal numbers
+    newDisplayValue = displayValue.replace(/.$/, buttonValue)
+  } else if (
+    ARITHMATIC_OPERATION_BUTTONS.includes(lastCharOfDisplay) &&
+    ARITHMATIC_OPERATION_BUTTONS.includes(buttonValue) &&
+    ![BUTTON.OP_MINUS].includes(buttonValue)
+  ) {
+    newDisplayValue = displayValue
+  } else if ([BUTTON.OP_AC].includes(buttonValue)) {
+    newDisplayValue = '0'
+  } else if ([BUTTON.OP_EQUALS].includes(buttonValue)) {
+    let mathString = displayValue
+    mathString = mathString.replaceAll(BUTTON.OP_DIVIDE, '/')
+    mathString = mathString.replaceAll(BUTTON.OP_MULTIPLY, '*')
+    mathString = mathString.replaceAll(BUTTON.OP_MINUS, '-')
+    mathString = mathString.replaceAll(BUTTON.OP_PLUS, '+')
+    let answer = eval(mathString)
+    newDisplayValue = `${answer}`
+  } else {
+    newDisplayValue = `${displayValue}${buttonValue}`
   }
 
-  // Handle operations
-  switch (buttonValue) {
-    case BUTTON.OP_AC: {
-      newDisplayValue = '0'
-      break
-    }
-    case BUTTON.OP_EQUALS: {
-      let mathString = displayValue
-      mathString = mathString.replaceAll(BUTTON.OP_DIVIDE, '/')
-      mathString = mathString.replaceAll(BUTTON.OP_MULTIPLY, '*')
-      mathString = mathString.replaceAll(BUTTON.OP_MINUS, '-')
-      mathString = mathString.replaceAll(BUTTON.OP_PLUS, '+')
-      let answer = eval(mathString)
-      newDisplayValue = `${answer}`
-      break
-    }
-    default: {
-      newDisplayValue = `${displayValue}${buttonValue}`
-    }
-  }
   setDisplayValue(newDisplayValue)
 }
